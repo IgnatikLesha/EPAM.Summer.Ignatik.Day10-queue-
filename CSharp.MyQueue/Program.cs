c0e21bc45c7a5060b573bd1bd176a1af4bfd24c5
Alexey Ignatik <ignatiklesha@gmail.com>
2016-07-20 18:46:37 +0300

    Add project files.

---
+++ CSharp.MyQueue/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.MyQueue
+{
+
+    public class Queue<T>: IEnumerable<T>
+    {
+        private T[] queueArray ;
+        private int First { get; set; }
+        private int Last { get; set; }
+        private const int InitCapacity = 16;
+        private int QueueCapacity;
+        private int index = 0;
+
+        //public Queue()
+        //{
+        //    queueArray = new T[InitCapacity];
+        //    First = Last =  0;
+        //}
+
+        public Queue(int length)
+        {
+            First = 0;
+            Last = 0;
+            QueueCapacity = length;
+            queueArray = new T[length];
+        }
+
+        public void Enqueue(T obj)
+        {
+
+            if ((Last++) % QueueCapacity == First % QueueCapacity)
+            {
+                ExpandQueue();
+                Last = (Last++) % QueueCapacity;
+                queueArray[Last] = obj;
+                QueueCapacity++;
+            }
+            else
+            {        
+                Last = (Last++) % QueueCapacity;
+                queueArray[Last] = obj;
+                QueueCapacity++;
+            }
+
+        }
+
+        private void ExpandQueue()
+        {
+            T[] NewArray = new T[queueArray.Length * 2];
+            for (int i = 0; i < QueueCapacity; i++)
+            {
+                NewArray[i] = queueArray[First];
+                First = (First++) % queueArray.Length;
+            }
+            queueArray = NewArray;
+            First = 0;
+            Last = QueueCapacity - 1;
+            QueueCapacity*=2;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+                throw new Exception(string.Format("Queue is empty!"));
+            else
+            {          
+                T value = queueArray[First];   
+                queueArray[First] = default(T);
+                First = (First++) % QueueCapacity;
+                QueueCapacity--;
+                return value;
+            }
+        }
+
+
+        public bool IsEmpty() {
+            if (QueueCapacity == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public void Clear()
+        {
+            First = 0;
+            Last = 0;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+                throw new Exception(string.Format("Queue is empty!"));
+            else
+                return queueArray[First];
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new QIterator<T>(queueArray);
+        }
+
+
+
+
+
+        }
+    }
+
---
+++ CSharp.MyQueue/QIterator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CSharp.MyQueue
+{
+    class QIterator<T>: IEnumerator<T>
+    {
+
+
+            public T[] arr;
+            int position = -1;
+
+            public bool MoveNext()
+            {
+                if (position == arr.Length - 1)
+                {
+                    Reset();
+                    return false;
+                }
+                position++;
+                return true;
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    return arr[position];
+                }
+            }
+
+            public T Current
+            {
+                get
+                {
+                    return arr[position];
+                }
+            }
+
+
+            public QIterator(T[] list)
+            {
+                arr = list;
+            }
+
+            public void Dispose() { }
+    
+    }
+}
